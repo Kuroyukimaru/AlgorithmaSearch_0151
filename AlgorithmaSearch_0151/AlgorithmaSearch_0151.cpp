63e82a9afc75e15d9368435bce0fd4aceff27910
Kuroyukimaru <oranggabut712@gmail.com>
2024-04-04 11:26:41 +0700

    membuat perintah element arraynya brp

--- AlgorithmaSearch_0151/AlgorithmaSearch_0151.cpp
+++ AlgorithmaSearch_0151/AlgorithmaSearch_0151.cpp
@@ -58,7 +58,10 @@ void LinearSearch() {
 }
 
 void display() {
-
+    cout << "Array elements are : ";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
 int main()
 {
